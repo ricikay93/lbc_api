d0ebe1790b51df9bbe1393494427e3cb3527f3b6
dcstewart <ricikay1993@gmail.com>
2018-04-13 07:01:46 -0500

    fix to ensure that if keys query parameter is passed other filters arent applied

--- v1/controllers/parse_parameters.js
+++ v1/controllers/parse_parameters.js
@@ -11,49 +11,45 @@ exports.parse_parameters = function(req) {
 
     console.log('Attributes' + req.query.keys);
 
-
-    // if searching for keys all others are irrevalent so the params are returned.
-    if (req.query.keys !== undefined && req.query.keys !== null) {
-        params["ids"] = param_attr_spliter(req.query.keys).forEach(item => {
-            return parseInt(item, 10);
-        });
-
-        return params;
-    }
-
-    if (req.query.attributes !== undefined && req.query.attributes !== null) {
-        params["attributes"] = param_attr_spliter(req.query.attributes);
-    }
-
-    if (req.query.filter !== undefined && req.query.filter !== null) {
-        params["filters"] = param_attr_spliter(req.query.filter);
-
-
-    }
-    if (req.query.q !== undefined && req.query.q !== null) {
-        var tmp_params = {};
-
-        tmp_params["value"] = param_attr_spliter(req.query.q);
-        if (req.query.search !== undefined && req.query.search !== null) {
-            tmp_params["search"] = param_attr_spliter(req.query.search);
+    try {
+        if (req.query.attributes !== undefined && req.query.attributes !== null) {
+            params["attributes"] = param_attr_spliter(req.query.attributes);
         }
 
-        params["q"] = [];
-        params.q[0] = tmp_params;
-    }
+        if (req.query.page !== undefined && req.query.page !== null) {
+            params["page"] = parseInt(req.query.page);
+            if (req.query.limit !== undefined && req.query.limit !== null) {
+                params["limit"] = parseInt(req.query.limit);
+            } else {
+                params["limit"] = 40;
+            }
 
-    if (req.query.page !== undefined && req.query.page !== null) {
-        params["page"] = parseInt(req.query.page);
-        if (req.query.limit !== undefined && req.query.limit !== null) {
+        } else if (req.query.limit !== undefined && req.query.limit !== null) {
             params["limit"] = parseInt(req.query.limit);
-        } else {
-            params["limit"] = 40;
         }
 
-    } else if (req.query.limit !== undefined && req.query.limit !== null) {
-        params["limit"] = parseInt(req.query.limit);
-    }
+        // if searching for keys all others are irrevalent so the params are returned.
+        if (req.query.keys !== undefined && req.query.keys !== null) {
+            params["keys"] = param_attr_spliter(req.query.keys);
+        } else {
+            if (req.query.filter !== undefined && req.query.filter !== null) {
+                params["filters"] = param_attr_spliter(req.query.filter);
 
+
+            }
+            if (req.query.q !== undefined && req.query.q !== null) {
+                var tmp_params = {};
+
+                tmp_params["value"] = param_attr_spliter(req.query.q);
+                if (req.query.search !== undefined && req.query.search !== null) {
+                    tmp_params["search"] = param_attr_spliter(req.query.search);
+                }
+
+                params["q"] = [];
+                params.q[0] = tmp_params;
+            }
+        }
+    } catch (err) {}
 
     return params;
 }
