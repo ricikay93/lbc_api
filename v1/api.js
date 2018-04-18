0be4bea7b5965fca64929da05c0e956428a95a1e
dcstewart <ricikay1993@gmail.com>
2018-04-18 07:17:35 -0500

    include church as an endpoint to connect to

--- v1/api.js
+++ v1/api.js
@@ -2,13 +2,14 @@ const router = require('express').Router();
 const references = require('./routes/references');
 // const churches = require('./routes/church');
 const circuit = require('./routes/circuit');
+const churches = require('./routes/church');
 // const visitor = require('./visitor');
 // const api = require('./api');
 
 router.use('/lookup', references);
 // router.use('/churches', churches);
 router.use('/circuits', circuit);
-// router.use('/churches', church);
+router.use('/churches', church);
 // router.use('/visitors', visitor);
 
 module.exports = router
